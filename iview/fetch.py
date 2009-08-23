a00c0e553d48b41d37e9f99eaee704a59960e646
Jeremy Visser <jeremy@visser.name>
2009-08-23 23:11:21 +1000

    Make compatible with flvstreamer Debian package. (Debian renames the executable.)

--- iview/fetch.py
+++ iview/fetch.py
@@ -2,9 +2,9 @@ import comm
 import os
 import subprocess
 
-def flvstreamer_x86(rtmp_host, rtmp_app, rtmp_playpath, output_filename, resume=False, execvp=False):
+def flvstreamer(rtmp_host, rtmp_app, rtmp_playpath, output_filename, resume=False, execvp=False):
 	args = [
-			'flvstreamer_x86',
+			'flvstreamer',
 			'--host', rtmp_host,
 			'--app',  rtmp_app,
 			'--playpath', rtmp_playpath,
@@ -15,7 +15,10 @@ def flvstreamer_x86(rtmp_host, rtmp_app, rtmp_playpath, output_filename, resume=
 		args.append('--resume')
 
 	if execvp:
-		os.execvp(args[0], args)
+		try:
+			os.execvp('flvstreamer_x86', args) # the upstream naming convention
+		except OSError:
+			os.execvp('flvstreamer', args)     # the Debian naming convention
 	else:
 		return subprocess.Popen(args, stderr=subprocess.PIPE)
 
@@ -24,7 +27,7 @@ def fetch_program(url, execvp=False):
 	resume = os.path.isfile(filename)
 	auth = comm.get_auth()
 
-	return flvstreamer_x86(
+	return flvstreamer(
 			auth['rtmp_host'],
 			auth['rtmp_app'] + '?auth=' + auth['token'],
 			url,
